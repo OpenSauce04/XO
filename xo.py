8538f8fa049a4808a21e0aaa286dcbe87c4215ec
Daniel Bradbury <danieljamesbradbury@hotmail.com>
2021-09-15 09:31:14 +0100

    Now correctly handles cases where nobody wins; Invalid input messages no longer stack

--- xo.py
+++ xo.py
@@ -4,55 +4,79 @@ from random import randint # Used to pick moves for bot opponent
 
 from wincons import wincons # Win condition arrays
 
-board = ["", ' ',' ',' ',' ',' ',' ',' ',' ',' ',] #Skips value 0
+while True: # Main loop
+  system("cls")
+  board = ["", ' ',' ',' ',' ',' ',' ',' ',' ',' ',] #Skips value 0
+  winner = ''
+  def drawBoard():
+    print(" " + board[7] + ' ║ ' + board[8] + ' ║ ' + board[9]) #There is definitely a better way of doing this
+    print("═══╬═══╬═══")
+    print(" " + board[4] + ' ║ ' + board[5] + ' ║ ' + board[6])
+    print("═══╬═══╬═══")
+    print(" " + board[1] + ' ║ ' + board[2] + ' ║ ' + board[3])
 
-def drawBoard():
-  print(" " + board[7] + ' ║ ' + board[8] + ' ║ ' + board[9]) #There is definitely a better way of doing this
-  print("═══╬═══╬═══")
-  print(" " + board[4] + ' ║ ' + board[5] + ' ║ ' + board[6])
-  print("═══╬═══╬═══")
-  print(" " + board[1] + ' ║ ' + board[2] + ' ║ ' + board[3])
-
-while True:
-  drawBoard()
-
-  # Player Turn
-  print("Press your selected space on the number pad")
   while True:
-    try:
-      userinput = int(str(getch())[2]) # Get entered character
+    drawBoard()
 
-      if board[userinput] == ' ':
-        board[userinput] = "X" # Player is ALWAYS X
-        break
-      else:
-        print("Space is taken")
+    # Player Turn
+    print("Press your selected space on the number pad")
+    while True:
+      try:
+        userinput = int(str(getch())[2]) # Get entered character
 
-    except:
-      print("Invalid input")
-      continue
-  # Bot turn
-  botinput = randint(1,9)
-  while board[botinput] != ' ':
+        if board[userinput] == ' ':
+          board[userinput] = "X" # Player is ALWAYS X
+          break
+        else:
+          system("cls")
+          drawBoard()
+          print("Press your selected space on the number pad")
+          print("Space is taken")
+
+      except:
+        system("cls")
+        drawBoard()
+        print("Press your selected space on the number pad")
+        print("Invalid input")
+        continue
+
+    # Bot turn
     botinput = randint(1,9)
-  board[botinput] = 'O' # Bot is ALWAYS O
-  for p in ['X','O']: # Player
-    for w in range(0,len(wincons)-1): # Win condition index
-      matches = 0
-      for x in range(1,9): # Board position
-        if (board[x]==p) and (wincons[w][x-1]==1):
-          matches += 1
+    botattempts = 0
+    while board[botinput] != ' ':
+      botinput = randint(1,9)
+      botattempts+=1
+      if botattempts>100:
+        break
+    if not botattempts>100:
+      board[botinput] = 'O' # Bot is ALWAYS O
+
+
+    # Checking for a win condition
+    for p in ['X','O']: # Player
+      for w in range(0,len(wincons)-1): # Win condition index
+        matches = 0
+        for x in range(1,9): # Board position
+          if (board[x]==p) and (wincons[w][x-1]==1):
+            matches += 1
+        if matches == 3:
+          break
       if matches == 3:
         break
     if matches == 3:
+      winner = p
       break
-  if matches == 3:
-    winner = p
-    break
-  system("cls")
+    if not ' ' in board:
+      break
+    system("cls")
 
-# Someone has won!
-system("cls")
-drawBoard()
-print(winner + " WON!")
-input()
+  # The match has ended
+  system("cls")
+  drawBoard()
+  if winner == '':
+    print("Nobody won!")  
+  else:
+    print(winner + " won!")
+
+  print("\nPress any key to try again")
+  getch()
